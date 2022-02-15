903abc8c17488a89047a03cfd52a07c971e2f145
Todd Langford-Archer <toddpla@gmail.com>
2022-02-15 23:21:53 +0000

    Cloud plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-gatsby-cloud`,
     `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,
